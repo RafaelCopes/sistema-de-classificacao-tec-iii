0dd984bf7896049524cd26b2d6ed645d7c6612d7
RafaelCopes <rafaelcopes16@gmail.com>
2024-07-16 22:55:21 -0300

    add doctorate classification

--- sistema-classificao.py
+++ sistema-classificao.py
@@ -11,6 +11,12 @@ class Student:
         self.publication_scores = []
         self.publication_total_score = 0
         self.final_score = 0
+        self.project_score = 0
+        self.memorial_score = 0
+        self.interview_score = 0
+        self.final_doctorate_score = 0
+        self.inscricao_type = kwargs.get('Tipo de inscrição', '').lower()
+        
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -51,12 +57,28 @@ class Student:
             "B1": 2.50, "B2": 1.00, "B3": 0.50, "B4": 0.25, "sem qualis": 0.20
         }
 
-        # 3 para mestrado
-        publications = [
-            {'qualis': getattr(self, 'Qualis do local de publicação', ''), 'first_author': getattr(self, 'Primeiro autor', '') == 'Sim'},
-            {'qualis': getattr(self, 'Qualis do local de publicação.1', ''), 'first_author': getattr(self, 'Primeiro autor.1', '') == 'Sim'},
-            {'qualis': getattr(self, 'Qualis do local de publicação.2', ''), 'first_author': getattr(self, 'Primeiro autor.2', '') == 'Sim'}
-        ]
+        # Determinar o número de publicações a considerar
+        if self.inscricao_type == 'mestrado':
+            num_publications = 3
+        elif self.inscricao_type == 'doutorado':
+            num_publications = 5
+        else:
+            num_publications = 0
+
+        publications = []
+
+        # Adicionar a primeira publicação sem sufixo numérico
+        publications.append({
+            'qualis': getattr(self, 'Qualis do local de publicação', ''),
+            'first_author': getattr(self, 'Primeiro autor', '') == 'Sim'
+        })
+
+        # Adicionar as demais publicações com sufixo numérico
+        for i in range(1, num_publications):
+            publications.append({
+                'qualis': getattr(self, f'Qualis do local de publicação.{i}', ''),
+                'first_author': getattr(self, f'Primeiro autor.{i}', '') == 'Sim'
+            })
 
         # Calcular pontuação das publicações
         for pub in publications:
@@ -66,16 +88,33 @@ class Student:
                 if not pub['first_author']:
                     score /= 3
                 self.publication_scores.append(score)
+        
         # Calcular pontuação total das publicações        
         if self.publication_scores:
             self.publication_total_score = sum(self.publication_scores) / len(self.publication_scores)
         else:
             self.publication_total_score = 0
 
+    # Calcular pontuação do projeto, memorial e entrevista
+    def calculate_doctorate_scores(self, project_scores, memorial_scores, interview_scores):
+        self.project_score = sum(project_scores) / len(project_scores)
+        self.memorial_score = sum(memorial_scores) / len(memorial_scores)
+        self.interview_score = sum(interview_scores) / len(interview_scores)
+        self.final_doctorate_score = (self.project_score + self.memorial_score + self.interview_score) / 3
+
     # Calcular nota final
     def calculate_final_score(self):
-        # Nota final é a média entre a pontuação média e a pontuação total das publicações
-        self.final_score = round((self.average_score + self.publication_total_score) / 2, 2)
+        # Se mestrado, a nota final é a média entre a pontuação média e a pontuação total das publicações
+        if self.inscricao_type == 'mestrado':
+            self.final_score = round((self.average_score + self.publication_total_score) / 2, 2)
+        
+        # Se doutorado, a nota final é a média ponderada entre a pontuação média, pontuação total das publicações e a pontuação final do doutorado
+        if self.inscricao_type == 'doutorado':
+            self.final_score = round((
+                (self.average_score * 2) + 
+                (self.publication_total_score * 5) + 
+                (self.final_doctorate_score * 3)
+            ) / 10, 2)
 
     def print_scores(self):
         print(f"Nome: {self.get_name()}")
@@ -83,7 +122,13 @@ class Student:
         for i, score in enumerate(self.publication_scores):
             print(f"  Nota da Publicação {i + 1}: {score:.2f}")
         print(f"  Nota Final das Publicações: {self.publication_total_score:.2f}")
+        if self.inscricao_type == 'doutorado':
+            print(f"  Projeto de Doutorado: {self.project_score:.2f}")
+            print(f"  Memorial Acadêmico: {self.memorial_score:.2f}")
+            print(f"  Entrevista: {self.interview_score:.2f}")
+        
         print(f"  Nota Final: {self.final_score:.2f}")
+        print("\n")
 
     def get_publication_scores(self):
         return [f"Nota da Publicação {i + 1}: {score:.2f}" for i, score in enumerate(self.publication_scores)]
@@ -156,41 +201,72 @@ class StudentFactory:
             students.append(student)
         return students
 
+def get_masters_scores(student, inscricoes_df, historico_df):
+    # Obter tipo de curso
+    inscricao = inscricoes_df[inscricoes_df['CPF'] == student.CPF]
+    course_type = inscricao.iloc[0]['Tipo de curso']
+    
+    # Obter média do histórico
+    historico = historico_df[historico_df['CPF'] == student.CPF]
+    if not historico.empty:
+        average = historico.iloc[0]['Media Historico']
+        student.calculate_average_score(average, course_type)
+    # Calcular pontuação das publicações
+    student.calculate_publication_scores()
+    student.calculate_final_score()
+
+def get_doctorate_scores(student, inscricoes_df, historico_df, avaliacoes_df):
+    # Obter tipo de curso
+    inscricao = inscricoes_df[inscricoes_df['CPF'] == student.CPF]
+    course_type = inscricao.iloc[0]['Tipo de curso']
+    
+    # Obter média do histórico
+    historico = historico_df[historico_df['CPF'] == student.CPF]
+    if not historico.empty:
+        average = historico.iloc[0]['Media Historico']
+        student.calculate_average_score(average, course_type)
+    # Calcular pontuação das publicações
+    student.calculate_publication_scores()
+    
+    # Obter notas de projeto, memorial e entrevista
+    avaliacoes = avaliacoes_df[avaliacoes_df['CPF'] == student.CPF]
+    if not avaliacoes.empty:
+        project_scores = avaliacoes[['Nota projeto 1', 'Nota projeto 2', 'Nota projeto 3']].values.flatten().tolist()
+        memorial_scores = avaliacoes[['Nota memorial 1', 'Nota memorial 2', 'Nota memorial 3']].values.flatten().tolist()
+        interview_scores = avaliacoes[['Nota entrevista 1', 'Nota entrevista 2', 'Nota entrevista 3']].values.flatten().tolist()
+        student.calculate_doctorate_scores(project_scores, memorial_scores, interview_scores)
+    
+    student.calculate_final_score()
+
 # Processa os alunos, calcula as pontuações
-def process_students(inscricoes_df, historico_df, name_attribute):
+def process_students(inscricoes_df, historico_df, avaliacoes_df, name_attribute):
     if 'Tipo de inscrição' not in inscricoes_df.columns:
         raise KeyError("'Tipo de inscrição' column not found in inscricoes_df")
     
     if 'Tipo de curso' not in inscricoes_df.columns:
         raise KeyError("'Tipo de curso' column not found in inscricoes_df")
 
-    # Selecionar alunos de mestrado
+    # Selecionar alunos de mestrado e doutorado
     mestrado_df = inscricoes_df[inscricoes_df['Tipo de inscrição'].str.lower() == 'mestrado']
+    doutorado_df = inscricoes_df[inscricoes_df['Tipo de inscrição'].str.lower() == 'doutorado']
     
-    students = StudentFactory.create_students_from_dataframe(mestrado_df, name_attribute)
+    students_mestrado = StudentFactory.create_students_from_dataframe(mestrado_df, name_attribute)
+    students_doutorado = StudentFactory.create_students_from_dataframe(doutorado_df, name_attribute)
     downloader = FileDownloader()
-    
-    for student in students:
-        # Obter tipo de curso
-        inscricao = inscricoes_df[inscricoes_df['CPF'] == student.CPF]
-        course_type = inscricao.iloc[0]['Tipo de curso']
-        
-        # Obter média do histórico
-        historico = historico_df[historico_df['CPF'] == student.CPF]
-        if not historico.empty:
-            average = historico.iloc[0]['Media Historico']
-            student.calculate_average_score(average, course_type)
-        # Calcular pontuação das publicações
-        student.calculate_publication_scores()
-        student.calculate_final_score()
-    
-    return students
 
-# Salva os resultados em um arquivo CSV
-def save_results(students, output_file):
-    # Criar DataFrame com resultados
-    results = []
-    for student in students:
+    for student in students_mestrado:
+        get_masters_scores(student, inscricoes_df, historico_df)
+
+    for student in students_doutorado:
+        get_doctorate_scores(student, inscricoes_df, historico_df, avaliacoes_df)
+
+    return students_mestrado, students_doutorado
+
+# Salva os resultados em arquivos CSV
+def save_results(students_mestrado, students_doutorado, output_file_mestrado, output_file_doutorado):
+    # Criar DataFrame com resultados dos alunos de mestrado
+    results_mestrado = []
+    for student in students_mestrado:
         result = {
             'Nome': student.get_name(),
             'CPF': student.CPF,
@@ -198,17 +274,40 @@ def save_results(students, output_file):
         }
         for i, score in enumerate(student.publication_scores):
             result[f'Nota da Publicação {i + 1}'] = f"{score:.2f}"
-        results.append(result)
+        results_mestrado.append(result)
 
         result['Nota Final Publicações'] = f"{student.publication_total_score:.2f}"
         result['Nota Final'] = f"{student.final_score:.2f}"
     
-    results_df = pd.DataFrame(results)
-    results_df = results_df[results_df['Nota Final'] >= '1.50']
-    results_df.sort_values(by='Nota Final', ascending=False, inplace=True)
+    results_mestrado_df = pd.DataFrame(results_mestrado)
+    results_mestrado_df = results_mestrado_df[results_mestrado_df['Nota Final'].astype(float) >= 1.50]
+    results_mestrado_df.sort_values(by='Nota Final', ascending=False, inplace=True)
+    results_mestrado_df.to_csv(output_file_mestrado, index=False)
+    print(f"Resultados de mestrado salvos em '{output_file_mestrado}'")
+
+    # Criar DataFrame com resultados dos alunos de doutorado
+    results_doutorado = []
+    for student in students_doutorado:
+        result = {
+            'Nome': student.get_name(),
+            'CPF': student.CPF,
+            'Pontuacao Histórico': f"{student.average_score:.2f}",
+            'Projeto de Doutorado': f"{student.project_score:.2f}",
+            'Memorial Acadêmico': f"{student.memorial_score:.2f}",
+            'Entrevista': f"{student.interview_score:.2f}"
+        }
+        for i, score in enumerate(student.publication_scores):
+            result[f'Nota da Publicação {i + 1}'] = f"{score:.2f}"
+        results_doutorado.append(result)
+
+        result['Nota Final Publicações'] = f"{student.publication_total_score:.2f}"
+        result['Nota Final'] = f"{student.final_score:.2f}"
     
-    results_df.to_csv(output_file, index=False)
-    print(f"Resultados salvos em '{output_file}'")
+    results_doutorado_df = pd.DataFrame(results_doutorado)
+    results_doutorado_df = results_doutorado_df[results_doutorado_df['Nota Final'].astype(float) >= 1.50]
+    results_doutorado_df.sort_values(by='Nota Final', ascending=False, inplace=True)
+    results_doutorado_df.to_csv(output_file_doutorado, index=False)
+    print(f"Resultados de doutorado salvos em '{output_file_doutorado}'")
 
 def print_student_scores(students):
     for student in students:
@@ -218,15 +317,17 @@ def print_student_scores(students):
 def main():
     inscricoes_path = 'inscricoes.csv'
     historico_path = 'historico.csv'
+    avaliacoes_path = 'avaliacoes.csv'
     name_attribute = 'Nome Completo'
     
     inscricoes_df = CSVReader.read_csv_to_dataframe(inscricoes_path)
     historico_df = CSVReader.read_csv_to_dataframe(historico_path)
+    avaliacoes_df = CSVReader.read_csv_to_dataframe(avaliacoes_path)
     
-    if inscricoes_df is not None and historico_df is not None:
-        students = process_students(inscricoes_df, historico_df, name_attribute)
-        save_results(students, 'resultados_mestrado.csv')
-        print_student_scores(students)
+    if inscricoes_df is not None and historico_df is not None and avaliacoes_df is not None:
+        students_mestrado, students_doutorado = process_students(inscricoes_df, historico_df, avaliacoes_df, name_attribute)
+        save_results(students_mestrado, students_doutorado, 'resultados_mestrado.csv', 'resultados_doutorado.csv')
+        #print_student_scores(students_mestrado + students_doutorado)
 
 if __name__ == "__main__":
     main()
